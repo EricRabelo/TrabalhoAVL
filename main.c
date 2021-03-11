c96ea4055b5008fcd373d3879f62241c59d37711
luizgfalqueto <lgfalqueto85@gmail.com>
2021-03-11 10:27:57 -0300

    falta implementar

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+TRABALHO DE IMPLEMENTACAO DE ARVORE BINARIA DE BUSCA BALANCEADA (AVL)
+AUTOR: LUIZ GUSTAVO FALQUETO
+DATA CRIACAO: 11/03/2021
+ARQUIVO: main.c
+VERSAO: 1.0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArvBinAvl.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
